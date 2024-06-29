fee4c593fd0c8a48d1b3779fbe5ca99d1ff8b8d8
rakib <phi.rakib@gmail.com>
2024-06-29 12:22:39 +0600

    changed home routes for products

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Livewire\Products\Edit as ProductEdit;
 use App\Livewire\Products\Index as ProductIndex;
 use Illuminate\Support\Facades\Route;
 
-Route::get('products', ProductIndex::class)->name('products.index');
+Route::get('/', ProductIndex::class)->name('products.index');
 Route::get('products/create', ProductCreate::class)->name('products.create');
 Route::get('products/edit/{id}', ProductEdit::class)->name('products.edit');
