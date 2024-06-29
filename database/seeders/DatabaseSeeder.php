12fc451536e7c88bce4fedadf70eb6445bcf9adc
rakib <phi.rakib@gmail.com>
2024-06-29 18:50:31 +0600

    added brand model, factory, seeder & migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            BrandSeeder::class,
             ProductSeeder::class,
         ]);
     }
