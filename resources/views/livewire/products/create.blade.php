efa7353663ef58b1f174326705ff4467d4cff4a7
rakib <phi.rakib@gmail.com>
2024-06-29 12:43:30 +0600

    added product page title header, back button & styles

--- resources/views/livewire/products/create.blade.php
+++ resources/views/livewire/products/create.blade.php
@@ -1,21 +1,35 @@
-<div>
-    <form wire:submit.prevent="submit">
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" id="name" class="form-control" wire:model="form.name">
-            @error('form.name')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
+<div class="container my-4">
+    <div class="row">
+        <div class="col-12">
+            <div class="float-left">
+                <h2>Product Create</h2>
+            </div>
+            <div class="float-right">
+                <a href="{{ route('products.index') }}" class="btn btn-primary">Back</a>
+            </div>
         </div>
-
-        <div class="form-group">
-            <label for="price"">Price</label>
-            <input type="text" id="price" class="form-control" wire:model="form.price">
-            @error('form.price')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <form wire:submit.prevent="submit">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" class="form-control" wire:model="form.name">
+                    @error('form.name')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+        
+                <div class="form-group">
+                    <label for="price"">Price</label>
+                    <input type="text" id="price" class="form-control" wire:model="form.price">
+                    @error('form.price')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+        
+                <button type="submit" class="btn btn-success">Create</button>
+            </form>
         </div>
-
-        <button type="submit" class="btn btn-success">Create</button>
-    </form>
+    </div>
 </div>
