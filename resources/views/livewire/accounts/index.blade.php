09cf3cbb2f92304e3c89c88997f8c2735690e745
rakib <phi.rakib@gmail.com>
2024-06-30 11:30:03 +0600

    added account create link & fixed create button position

--- resources/views/livewire/accounts/index.blade.php
+++ resources/views/livewire/accounts/index.blade.php
@@ -1,10 +1,12 @@
 <div class="container my-4">
     <div class="row my-4">
-        <div class="float-left">
-            <h2>Accounts Index</h2>
-        </div>
-        <div class="float-right">
-            <a href="#" class="btn btn-primary">Create</a>
+        <div class="col-12">
+            <div class="float-left">
+                <h2>Accounts Index</h2>
+            </div>
+            <div class="float-right">
+                <a href="{{ route('accounts.create') }}" class="btn btn-primary">Create</a>
+            </div>
         </div>
     </div>
 
