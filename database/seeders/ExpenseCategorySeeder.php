b142dd11fef386dc37096c98eccb8bd84a72301b
rakib <phi.rakib@gmail.com>
2024-07-01 12:51:06 +0600

    added expense category model, factory, seeder & migration

---
+++ database/seeders/ExpenseCategorySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ExpenseCategory;
+use Illuminate\Database\Seeder;
+
+class ExpenseCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        ExpenseCategory::factory(5)->create();
+    }
+}
